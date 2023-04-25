f7f13e463df02e7ebbf529fcf436a21d2abc976f
Maximo Granda <93052286+maximograndap@users.noreply.github.com>
2023-04-25 11:27:00 -0500

    Add files via upload

---
+++ Mascota.class.php
@@ -0,0 +1,12 @@
+<?php
+
+class Mascota {
+	public $id;
+	public $nombre;
+	public $tipo;
+	public $edad;
+	public $peso;
+	public $fecha_nacimiento;
+}
+
+?>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once './Slim/Slim.php';
 require_once 'Conexion.class.php';
 require_once 'Producto.class.php';
+require_once 'Mascota.class.php';
 require_once 'ProductoDAO.class.php';
 
 
@@ -19,10 +20,24 @@ $app->get('/productos', function(){
     echo json_encode($lista);    
 });
 
+// Mascotas: Servicio 1
+$app->get('/mascotas', function(){  
+	$dao = new ProductoDAO();
+    $lista = $dao->listarMascota();    
+    echo json_encode($lista);    
+});
+
 // Productos: Servicio 2
 $app->get('/productos/:nombre', function($nombre){   
 	$dao = new ProductoDAO();
     $lista = $dao->buscarPorNombre($nombre); 	
+    echo json_encode($lista);    
+});
+
+// Mascotas: Servicio 2
+$app->get('/mascotas/:nombre', function($nombre){   
+	$dao = new ProductoDAO();
+    $lista = $dao->buscarPorNombreMascota($nombre); 	
     echo json_encode($lista);    
 });
 
@@ -38,6 +53,55 @@ $app->post('/productos', function () use ($app) {
    $dao = new ProductoDAO();
    $dao->insertar($objeto);   
    echo json_encode(array('mensaje' => "Producto registrado satisfactoriamente"));    
+});
+
+// Mascota: Servicio 3
+$app->post('/imascotas', function () use ($app) {    
+   $nombre = $app->request()->post('nombre');
+   $tipo = $app->request()->post('tipo'); 
+   $edad = $app->request()->post('edad'); 
+   $peso = $app->request()->post('peso'); 
+   $fecha_nacimiento = $app->request()->post('fecha_nacimiento'); 
+   $objeto = new Mascota(); 
+   $objeto->nombre = $nombre;
+   $objeto->tipo = $tipo;
+   $objeto->edad = $edad;
+   $objeto->peso = $peso;
+   $objeto->fecha_nacimiento = $fecha_nacimiento;
+   
+   $dao = new ProductoDAO();
+   $dao->insertarMascota($objeto);   
+   echo json_encode(array('mensaje' => "Mascota registrada satisfactoriamente"));    
+});
+
+// Mascota: Servicio 4
+$app->post('/umascotas', function () use ($app) {    
+   $nombre = $app->request()->post('nombre');
+   $tipo = $app->request()->post('tipo');
+   $edad = $app->request()->post('edad');
+   $peso = $app->request()->post('peso');
+   $fecha_nacimiento = $app->request()->post('fecha_nacimiento');
+   $id = $app->request()->post('id');
+   $objeto = new Mascota(); 
+   $objeto->nombre = $nombre;
+   $objeto->tipo = $tipo;
+   $objeto->edad = $edad;
+   $objeto->peso = $peso;
+   $objeto->fecha_nacimiento = $fecha_nacimiento;
+   $objeto->id = $id;
+   
+   $dao = new ProductoDAO();
+   $dao->actualizarMascota($objeto);   
+   echo json_encode(array('mensaje' => "Mascota actualizada satisfactoriamente"));    
+});
+
+// Mascota: Servicio 5
+$app->post('/dmascotas', function () use ($app) {
+   $id = $app->request()->post('id');
+   
+   $dao = new ProductoDAO();
+   $dao->eliminarMascota($id);   
+   echo json_encode(array('mensaje' => "Mascota eliminada satisfactoriamente"));    
 });
 
 // Avisos: Servicio 1
